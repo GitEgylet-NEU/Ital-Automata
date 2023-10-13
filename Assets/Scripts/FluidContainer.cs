0a7b33afd1492d6d1bd5d4f734b3452ab22f7de1
Hunor Csuhai <team3csh@gmail.com>
2023-10-13 20:09:44 +0200

    Half-working dynamic mesh
    Issue: area gets bigger as the container gets rotated.
    TODO: Calculate the triangle's vertices based on its angles and the desired area.

--- Assets/Scripts/FluidContainer.cs
+++ Assets/Scripts/FluidContainer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -8,7 +9,7 @@ public class FluidContainer : MonoBehaviour
 	[Min(0)] public float width = 1f;
 	[Min(0)] public float height = 1f;
 	[Min(0)] public float thickness = .1f;
-	[Range(0, 360)] public float rotation = 0f;
+	[Range(-180, 180)] public float rotation = 0f;
 	float capacity; // litres
 
 	[Min(0)] public float litres = 0f;
@@ -28,8 +29,6 @@ public class FluidContainer : MonoBehaviour
 
 		fluidMeshFilter = GetComponentInChildren<MeshFilter>();
 		fluidMeshRenderer = GetComponentInChildren<MeshRenderer>();
-		fluidMeshFilter.sharedMesh = new Mesh();
-		fluidMeshFilter.sharedMesh.name = "Fluid";
 	}
 
 	private void OnEnable()
@@ -40,6 +39,7 @@ public class FluidContainer : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		//container
 		lineRenderer.SetPosition(0, new Vector2(-(width + thickness), height + thickness));
 		lineRenderer.SetPosition(1, new Vector2(-(width + thickness), -(height + thickness)));
 		lineRenderer.SetPosition(2, new Vector2(width + thickness, -(height + thickness)));
@@ -53,44 +53,123 @@ public class FluidContainer : MonoBehaviour
 			litres = capacity;
 		}
 
-		float neededHeight = UnitConverter.MetricToUnity(UnitConverter.LitreToMetric(litres) / width);
 
-		Vector3[] vertices = new Vector3[]
+		float neededHeight = UnitConverter.MetricToUnity(UnitConverter.LitreToMetric(litres) / width);
+		//text.text = neededHeight.ToString();
+
+		float heightA = Mathf.Tan(rotation * Mathf.Deg2Rad) * (width / 2f);
+		Debug.Log("heightA=" + heightA);
+		float heightB = Mathf.Tan(-rotation * Mathf.Deg2Rad) * (width / 2f);
+
+		List<Vector3> vertices = new()
 		{
-			new Vector2(-width, neededHeight*2),
+			new Vector2(-width, neededHeight*2 - heightA*2),
 			new Vector2(-width, 0),
 			new Vector2(width, 0),
-			new Vector2(width, neededHeight*2)
+			new Vector2(width, neededHeight*2 - heightB*2)
 		};
-		int[] triangles = new int[]
+		if (heightA > neededHeight) //point A out of lower bounds
 		{
-			0, 1, 2,
-			0, 2, 3
-		};
-		Vector2[] uvs = new Vector2[]
+			vertices.RemoveAt(1);
+			vertices[0] = new Vector2(-Mathf.Tan(Mathf.Abs(90f - rotation) * Mathf.Deg2Rad) * neededHeight*2, 0);
+		}
+		if (heightB > neededHeight)
 		{
-			new Vector2(0, 1),
-			new Vector2(0, 0),
-			new Vector2(1, 0),
-			new Vector2(1, 1)
-		};
-		fluidMeshFilter.sharedMesh.vertices = vertices;
-		fluidMeshFilter.sharedMesh.triangles = triangles.Reverse().ToArray();
-		fluidMeshFilter.sharedMesh.uv = uvs;
-		fluidMeshFilter.sharedMesh.RecalculateBounds();
-		fluidMeshFilter.sharedMesh.RecalculateNormals();
-		fluidMeshRenderer.transform.localPosition = new Vector2(0, -height);
+			vertices.RemoveAt(2);
+			vertices[2] = new Vector2(Mathf.Tan(-Mathf.Abs(90f - rotation) * Mathf.Deg2Rad) * neededHeight * 2, 0);
+		}
+		fluidMeshFilter.sharedMesh = GenerateFluidMesh(vertices.ToArray());
 
+		float a = vertices[0].y/2f;
+		float b = (vertices.Last().x + 1f)/2f;
+		float c = Mathf.Sqrt(a*a + b*b);
+		Debug.Log($"a={a}; b={b}; c={c}");
+		Debug.Log("Area: " + (a * b / 2f));
+
+		text.text = UnitConverter.MetricToLitre(UnitConverter.UnityToMetric(fluidMeshFilter.sharedMesh.CalculateSurfaceArea()/4f)).ToString("0.##") + " l"; //current fluid area
+
+		fluidMeshRenderer.transform.localPosition = new Vector2(0, -height);
+		transform.rotation = Quaternion.Euler(0, 0, -rotation);
+			
+
+		//text
 		if (capacity == 0 || thickness == 0)
 		{
 			text.enabled = false;
 		}
 		else
 		{
-			text.text = UnitConverter.FormatVolume(litres) + " / " + UnitConverter.FormatVolume(capacity);
-			text.rectTransform.position = new Vector2(0, -(height / 2 + thickness) - .3f);
+			//text.text = UnitConverter.FormatVolume(litres) + " / " + UnitConverter.FormatVolume(capacity);
+			text.rectTransform.position = new Vector2(transform.position.x, transform.position.y-(height / 2 + thickness) - .3f);
 			text.rectTransform.sizeDelta = new Vector2(width + thickness, .3f);
 			text.enabled = true;
 		}
 	}
+
+	Mesh GenerateFluidMesh(Vector3[] vertices)
+	{
+		Mesh mesh = new();
+		mesh.name = "Fluid";
+		int[] triangles;
+		Vector2[] uvs;
+		if (vertices.Length > 3)
+		{
+			triangles = new int[]
+			{
+				0, 1, 2,
+				0, 2, 3
+			};
+			uvs = new Vector2[]
+			{
+				new Vector2(0, 1),
+				new Vector2(0, 0),
+				new Vector2(1, 0),
+				new Vector2(1, 1)
+			};
+		}
+		else
+		{
+			triangles = new int[]
+			{
+				0, 1, 2
+			};
+			uvs = new Vector2[]
+			{
+				new Vector2(0, 0),
+				new Vector2(1, 0),
+				new Vector2(1, 1)
+			};
+		}
+		mesh.vertices = vertices;
+		mesh.triangles = triangles.Reverse().ToArray();
+		mesh.uv = uvs;
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+		return mesh;
+	}
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Vector2 point = transform.GetChild(0).TransformPoint(fluidMeshFilter.sharedMesh.vertices[0]);
+		point = new(0, point.y);
+		Gizmos.DrawLine(point + new Vector2(-1, 0), point + new Vector2(1, 0));
+	}
 }
+
+public static class MeshExtensions
+{
+	public static float CalculateSurfaceArea(this Mesh mesh)
+	{
+		var triangles = mesh.triangles;
+		var vertices = mesh.vertices;
+		float sum = 0f;
+		for (int i = 0; i < triangles.Length; i += 3)
+		{
+			Vector3 corner = vertices[triangles[i]];
+			Vector3 a = vertices[triangles[i + 1]] - corner;
+			Vector3 b = vertices[triangles[i + 2]] - corner;
+			sum += Vector3.Cross(a, b).magnitude;
+		}
+		return sum / 2f;
+	}
+} 
