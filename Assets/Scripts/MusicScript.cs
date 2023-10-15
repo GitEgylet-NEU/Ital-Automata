9039ec88c469d13c566e628edb31749780eac04a
Dominik Gyönki <gyonkidominik@gmail.com>
2023-10-15 14:12:56 +0200

    Multiple choice with dropdown (faulty)
    Multiple choice music with dropdown.
    Changing music is not yet working due to a script error.

--- Assets/Scripts/MusicScript.cs
+++ Assets/Scripts/MusicScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MusicScript : MonoBehaviour
 {
@@ -8,11 +9,30 @@ public class MusicScript : MonoBehaviour
     bool musicPlaying = false;
     bool musicPaused = false;
     AudioSource Music;
+    TMPro.TMP_Dropdown Select;
 
     void Start()
     {
         Music = GetComponent<AudioSource>();
+        Select = GetComponent<TMPro.TMP_Dropdown>();
+        Music.clip = Resources.Load<AudioClip>("Audio/elevatormusic");
         Music.Stop();
+    }
+
+    public void ChangeMusic()
+    {
+        musicPlaying = false;
+        musicPaused = false;
+        Music.Stop();
+        var option = Select.options[Select.value].text; //NullReferenceException: Object reference not set to an instance of an object
+        if (option == "Local Forecast (Elevator)")
+        {
+            Music.clip = Resources.Load<AudioClip>("Audio/elevatormusic");
+        }
+        else if (option == "The Cannery")
+        {
+            Music.clip = Resources.Load<AudioClip>("Audio/thecannery");
+        }
     }
 
     public void StartStop()
