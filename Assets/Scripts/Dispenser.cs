a70ea72460b0ca90891f72889f48e00df41dc3ee
Hunor Csuhai <team3csh@gmail.com>
2023-10-17 20:08:59 +0200

    Basic dispenser script

---
+++ Assets/Scripts/Dispenser.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dispenser : MonoBehaviour
+{
+	[SerializeField] TMP_InputField atmeroInput, sebessegInput, idoInput;
+	[SerializeField] TextMeshProUGUI quantityText;
+	[SerializeField] Button dispenseButton;
+	float atmero, sebesseg, ido; //cm, cm, s - kimenetben dm-nek kell lennie (1u=1dm)
+	Transform waterFlow, spawnPoint;
+
+	[SerializeField] FluidContainer fluidContainer;
+	Coroutine dispenserCoroutine;
+
+	private void Awake()
+	{
+		atmeroInput.onEndEdit.AddListener((string v) =>
+		{
+			atmero = float.Parse(v);
+			transform.localScale = new Vector2((atmero + 1f) / 10f, 1);
+		});
+		sebessegInput.onEndEdit.AddListener((string v) => sebesseg = float.Parse(v));
+		idoInput.onEndEdit.AddListener((string v) => ido = float.Parse(v));
+		quantityText.text = string.Empty;
+		dispenseButton.onClick.AddListener(DispenseButton);
+		spawnPoint = transform.GetChild(0);
+		waterFlow = spawnPoint.GetChild(0);
+		waterFlow.gameObject.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (string.IsNullOrEmpty(atmeroInput.text) || string.IsNullOrEmpty(sebessegInput.text) || string.IsNullOrEmpty(idoInput.text) || dispenserCoroutine != null)
+		{
+			dispenseButton.interactable = false;
+		}
+		else
+		{
+			dispenseButton.interactable = true;
+		}
+	}
+
+	void DispenseButton()
+	{
+		float flow = QuantityCalculator.CalculateFlow(atmero / 2f, sebesseg) / 1000f; // liters/s
+		//Debug.Log($"flow: {flow} l/s");
+		float q = flow * ido;
+		quantityText.text = $"{Utils.FormatVolume(q)}\nflow: {Utils.FormatVolume(flow)}/s";
+		waterFlow.localScale = new Vector2(atmero / 10f, spawnPoint.transform.position.y - (fluidContainer.transform.position.y - fluidContainer.height/2f));
+		waterFlow.localPosition = new Vector2(0, -waterFlow.localScale.y / 2f);
+		waterFlow.gameObject.SetActive(true);
+		dispenserCoroutine = StartCoroutine(DispenseAmount(flow, ido));
+	}
+
+	IEnumerator DispenseAmount(float flow, float time)
+	{
+		float t = 0;
+		float original = fluidContainer.liters;
+		while (t <= time)
+		{
+			fluidContainer.liters += flow * Time.deltaTime;
+			t += Time.deltaTime;
+			yield return null;
+		}
+		fluidContainer.liters = original + time * flow;
+		quantityText.text = string.Empty;
+		waterFlow.gameObject.SetActive(false);
+		dispenserCoroutine = null;
+	}
+}
--- Assets/Scripts/QuantityCalculator.cs
+++ Assets/Scripts/QuantityCalculator.cs
@@ -88,12 +88,12 @@ public class QuantityCalculator : MonoBehaviour
 	/// radius and velocity must be in the same measurement! (cm, cm/s)
 	/// I=v*A (térfogat áramlása = áramlási sebesség * merõleges keresztmetszet)
 	/// </summary>
-	float CalculateFlow(float radius, float velocity)
+	public static float CalculateFlow(float radius, float velocity)
 	{
 		float area = Mathf.PI * radius * radius; //cm^2
 		return area * velocity; //(cm^3)/s
 	}
-	float CalculateQuantity(float radius, float velocity, float time)
+	public static float CalculateQuantity(float radius, float velocity, float time)
 	{
 		float flow = CalculateFlow(radius, velocity);
 		return flow * time; //ml=cm^3
