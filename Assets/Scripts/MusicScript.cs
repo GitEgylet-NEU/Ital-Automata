5480264ebb9d53e74d691c56481545d646d9655e
Dominik Gyönki <gyonkidominik@gmail.com>
2023-10-19 21:18:47 +0200

    Több zene és egy-két apróság
    Ha lejátszás közben van egy zene, akkor váltáskor a másik automatikusan elindul.

--- Assets/Scripts/MusicScript.cs
+++ Assets/Scripts/MusicScript.cs
@@ -3,35 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/*
+"Local Forecast - Elevator" Kevin MacLeod (incompetech.com)
+"The Cannery" Kevin MacLeod (incompetech.com)
+"Gaslamp Funworks" Kevin MacLeod (incompetech.com)
+"Stringed Disco" Kevin MacLeod (incompetech.com)
+Licensed under Creative Commons: By Attribution 4.0 License
+http://creativecommons.org/licenses/by/4.0/
+*/
+
 public class MusicScript : MonoBehaviour
 {
 
-    bool musicPlaying = false;
-    bool musicPaused = false;
+    bool musicPlaying;
+    bool musicPaused;
     AudioSource Music;
-    [SerializeField] TMPro.TMP_Dropdown Select; //a [SerializeField] tag-gel megjelenik a mező a Unity inspectorban, így bele lehet húzni dolgokat
+    [SerializeField] TMPro.TMP_Dropdown Select;
 
     void Start()
     {
         Music = GetComponent<AudioSource>();
-        //Select = GetComponent<TMPro.TMP_Dropdown>(); //a MusicScript GameObject-jén nincsen Dropdown komponens, tehát külön meg kell mondani a scriptnek, hogy melyikre gondolsz (lásd feljebb)
         Music.clip = Resources.Load<AudioClip>("Audio/elevatormusic");
-        Music.Stop();
+        Music.Play();
+        musicPlaying = true;
+        musicPaused = false;
     }
 
     public void ChangeMusic()
     {
-        musicPlaying = false;
         musicPaused = false;
         Music.Stop();
-        var option = Select.options[Select.value].text; //NullReferenceException: Object reference not set to an instance of an object
-        if (option == "Local Forecast (Elevator)")
+        var option = Select.options[Select.value].text;
+        switch (option)
         {
-            Music.clip = Resources.Load<AudioClip>("Audio/elevatormusic");
+            case "Local Forecast (Elevator)":
+                Music.clip = Resources.Load<AudioClip>("Audio/elevatormusic");
+                break;
+            case "The Cannery":
+                Music.clip = Resources.Load<AudioClip>("Audio/thecannery");
+                break;
+            case "Gaslamp Funworks":
+                Music.clip = Resources.Load<AudioClip>("Audio/gaslampfunworks");
+                break;
+            case "Stringed Disco":
+                Music.clip = Resources.Load<AudioClip>("Audio/stringeddisco");
+                break;
         }
-        else if (option == "The Cannery")
+        if (musicPlaying)
         {
-            Music.clip = Resources.Load<AudioClip>("Audio/thecannery");
+            Music.Play();
         }
     }
 
