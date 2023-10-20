9317319977400fcb6587097ecffc79127b0342d7
Dominik Gyönki <gyonkidominik@gmail.com>
2023-10-20 21:07:43 +0200

    Hangeffektek megkezdése
    Még FOLYAMATBAN van, NINCS készen! SoundEffects.cs kezeli a hangeffekteket. A legújabb MusicScriptemet és a zenéket átemeltem az audio branchról, mert innen eddig hiányoztak.

---
+++ Assets/Scripts/SoundEffects.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+    AudioSource Blugy = new();
+    AudioSource Brrrrr = new();
+    AudioSource Boop = new();
+    AudioSource Lotty = new();
+
+    private void Start()
+    {
+        Blugy.clip = Resources.Load<AudioClip>("SFX/blugy");
+        Brrrrr.clip = Resources.Load<AudioClip>("SFX/brrrrr");
+        Boop.clip = Resources.Load<AudioClip>("SFX/boop");
+        Lotty.clip = Resources.Load<AudioClip>("SFX/lotty");
+        UIcontroller.instance.onCalculateButtonPressed.AddListener(() => PlaySound("boop"));
+        UIcontroller.instance.onCalculateButtonPressed.AddListener(() => PlaySound("brrrrr"));
+        //Dispenser.instance.onButtonStateChanged.AddListener((bool x) => PlaySound("blugy"));
+        GyroscopeHandler.instance.gyroscopeButtonFlashed.AddListener(() => PlaySound("lotty"));
+    }
+
+    void PlaySound(string name)
+    {
+        switch (name)
+        {
+            case "blugy":
+                Blugy.Play();
+                break;
+            case "brrrrr":
+                Brrrrr.Play();
+                break;
+            case "boop":
+                Boop.Play();
+                break;
+            case "lotty":
+                Lotty.Play();
+                break;
+        }
+    }
+}
